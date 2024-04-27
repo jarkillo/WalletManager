725b3596b50c33ade91785eb558e987246dcc59e
Jarko <jarkoarenal@gmail.com>
2024-04-27 17:12:54 +0200

    Mas frontend

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import WalletInfo from './components/WalletInfo';
+import SendTransaction from './components/SendTransaction';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Wallet Manager</h1>
+            <WalletInfo />
+            <SendTransaction />
+        </div>
+    );
 }
 
 export default App;
