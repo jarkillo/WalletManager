e927eebd4dec132295a7825b207132567fa183db
Jarko <jarkoarenal@gmail.com>
2024-05-01 21:58:24 +0200

    Actualizacion de funcionalidades de la aplicacion
    - Añadida funcion para añadir o quitar tokens de la lista de tokens del balance (no funciona)
    - Ahora al consultar el saldo consulta tambien los LINKs que tenemos ademas del ETH
    - Falta añadir BTC a la lista de tokens
    - Al consultar el saldo ahora consulta una lista prefijada de tokens (SOlo esta añadido LINK de sepolia), además de ETH

--- Backend/models/schemas.py
+++ Backend/models/schemas.py
@@ -1,5 +1,5 @@
 # app/models/schemas.py
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, HttpUrl, Field
 
 class Transaction(BaseModel):
     signed_transaction: str
@@ -10,3 +10,17 @@ class Transaction(BaseModel):
         if not value.startswith('0x'):
             raise ValueError('La transacción firmada debe comenzar con 0x')
         return value
+    
+class Token(BaseModel):
+    token_name: str
+    token_address: str
+    network: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "token_name": "Dai Stablecoin",
+                "token_address": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+                "network": "mainnet"
+            }
+        }
---
+++ Frontend/src/components/TokenManager.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function TokenManager() {
+    const [tokenName, setTokenName] = useState('');
+    const [tokenAddress, setTokenAddress] = useState('');
+    const [network, setNetwork] = useState('mainnet'); // Valor predeterminado
+    const [message, setMessage] = useState('');
+
+    const handleAdd = async () => {
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/tokens/`, {
+                token_name: tokenName,
+                token_address: tokenAddress,
+                network: network
+            });
+            setMessage(response.data.msg);
+        } catch (error) {
+            setMessage('Error añadiendo el token: ' + (error.response?.data?.detail || 'Error desconocido'));
+        }
+    };
+
+    const handleDelete = async () => {
+        try {
+            const response = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/tokens/`, { data: { token_address: tokenAddress } });
+            setMessage(response.data.msg);
+        } catch (error) {
+            setMessage('Error eliminado el token: ' + (error.response?.data?.detail || 'Error desconocido'));
+        }
+    };
+
+    return (
+        <div>
+            <h2>Token Manager</h2>
+            <p>
+                <select value={network} onChange={(e) => setNetwork(e.target.value)}>
+                    <option value="mainnet">Mainnet</option>
+                    <option value="sepolia">Sepolia</option>
+                </select>
+            </p>
+            <p>
+                <input value={tokenName} onChange={(e) => setTokenName(e.target.value)} placeholder="Nombre del Token" />
+                <input value={tokenAddress} onChange={(e) => setTokenAddress(e.target.value)} placeholder="Dirección del token" />
+            </p>
+            <button onClick={handleAdd}>Añadir Token</button>
+            <button onClick={handleDelete}>Borrar Token</button>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default TokenManager;
+
+
