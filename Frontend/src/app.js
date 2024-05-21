3a3ab2b7d65cc64612716e94cafae0412bfd8f13
Jarko <jarkoarenal@gmail.com>
2024-05-21 14:40:44 +0200

    KAN-12 - Actualizacion de diseño Primeros cambios al diseño del Frontend Añadida imagen de fondo y modificado logo Añadido efecto 3d a los bloques

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -8,28 +8,30 @@ import TokenManager from './components/TokenManager';
 import TransactionDetails from './components/TransactionDetails';
 import TransactionRecords from './components/TransactionRecord';
 
-
 function App() {
     return (
-        <div className="container">
-
-            <div className="column">
-                <img src={logo} alt="Logo de la Empresa" className="App-logo" />
+        <div className="app-container">
+            <nav className="navbar">
+                <div className="logo-container">
+                    <img src={logo} alt="Logo de la Empresa" className="app-logo" />
+                    <span className="app-title">Wallet Manager</span>
+                </div>
+            </nav>
+            <div className="background-decor">
+                <div className="main-container">
+                    <div className="column">
+                        <CreateWallet />
+                        <TransactionDetails />
+                    </div>
+                    <div className="column">
+                        <WalletInfo />
+                        <TransactionRecords />
+                    </div>
+                    <div className="column">
+                        <SendTransaction />
+                    </div>
+                </div>
             </div>
-
-            <div className="column">
-                <CreateWallet />
-                <TokenManager />
-                <WalletInfo />
-            </div>
-
-            <div className="column">
-                <SendTransaction />
-                <TransactionDetails />
-                <TransactionRecords />
-
-            </div>
-
         </div>
     );
 }
